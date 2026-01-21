0085e0edccac091e19a69c9f3db1a416b4d0be22
VS-KOT <vinayakkotwala12@gmail.com>
2026-01-22 01:11:25 +0530

    Adding function for temprature over range

--- 01-python-basics/get_data.py
+++
@@ -1,5 +0,0 @@
-import requests
-from datetime import datetime, timedelta
-
-#timedelta is used to calcluate the date/time with specific difference provided by the user
-
--- 01-python-basics/temprature_project/services/temprature_api.py
+++ 01-python-basics/temprature_project/services/temprature_api.py
@@ -1,6 +1,7 @@
 import requests
 
-def get_weather(latitude, longitude):
+#current data {without date}
+def get_weather(latitude, longitude):                    
     url = (
         "https://api.open-meteo.com/v1/forecast"
         f"?latitude={latitude}"
@@ -15,3 +16,23 @@ def get_weather(latitude, longitude):
         "temperature": data["current"]["temperature_2m"],
         "wind_speed": data["current"]["wind_speed_10m"]
     }
+
+
+#range of date
+
+def range_of_data(latitude,longitude,start_Date,end_Date):
+
+    url = (
+    f"https://api.open-meteo.com/v1/forecast"
+    f"?latitude={latitude}"
+    f"&longitude={longitude}"
+    f"&hourly=temperature_2m"
+    f"&start_date={start_Date}"
+    f"&end_date={end_Date}"
+    )
+        
+    response = requests.get(url)
+    data = response.json()
+    
+    return data
+
